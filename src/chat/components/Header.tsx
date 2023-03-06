69eefb668f97a2d7efb612f738fa431b3d1edce3
FrancoMusolino <francomusolino55@gmail.com>
2023-03-06 12:33:56 -0300

    (home)[CN-38] page for no chat selected

--- src/chat/components/Header.tsx
+++ src/chat/components/Header.tsx
@@ -1,58 +1,12 @@
-import React from 'react'
-import { Heading, HStack, MenuItem } from '@chakra-ui/react'
+import { HStack } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
-import { CreateChat } from './CreateChat'
+export const Header = styled(HStack)`
+  height: 75px;
+  padding-block: 1rem;
+  padding-inline: 0.75rem;
 
-import { Avatar } from '@/shared/components/Avatar'
-import { Menu } from '@/shared/components/Menu'
-import { useGlobalDispatch, useStoreSelector } from '@/shared/app/store'
-import { useBrandTheme } from '@/shared/hooks'
-import { Alert } from '@/shared/components/Alert'
-import { Modal } from '@/shared/components/Modal'
-import { endSession } from '@/shared/features/session/session.actions'
-import { useQueryClient } from '@tanstack/react-query'
+  justify-content: space-between;
 
-export const Header = () => {
-  const { colors } = useBrandTheme()
-
-  const queryClient = useQueryClient()
-
-  const { username, profilePicture } = useStoreSelector('session')
-  const dispatch = useGlobalDispatch()
-
-  return (
-    <HStack
-      as='header'
-      h='75px'
-      paddingBlock={4}
-      paddingInline={3}
-      justify='space-between'
-      bgColor={colors.brand.secondary}
-    >
-      <HStack spacing={3}>
-        <Avatar src={profilePicture} />
-        <Heading fontSize='xl'>{username}</Heading>
-      </HStack>
-      <Menu>
-        <MenuItem>Configuración</MenuItem>
-        <Modal
-          trigger={MenuItem}
-          triggerText='Nuevo Grupo'
-          modalTitle='Crear Chat'
-        >
-          <CreateChat />
-        </Modal>
-        <Alert
-          trigger={MenuItem}
-          triggerText='Cerrar sesión'
-          alertTitle='¿Deseas cerrar tú sesión?'
-          btnText='Cerrar sesión'
-          action={() => {
-            dispatch(endSession())
-            queryClient.clear()
-          }}
-        />
-      </Menu>
-    </HStack>
-  )
-}
+  background-color: #202c33;
+`
